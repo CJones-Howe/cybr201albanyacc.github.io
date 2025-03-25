7ae8a3d5b0de7e947f0d57df603c5c54a6b12ccf
albanyacc <salgabyali@albany.edu>
2025-03-24 20:12:02 -0400

    Add files via upload
    all 6 in class work

---
+++ output.js
@@ -0,0 +1,15 @@
+function showInnerHTML()  {
+   document.getElementById("output1").innerHTML = "Inner Html Test!";
+}
+
+function useDocumentWrite() {
+   document.write("you cooked the page.");
+}
+
+function showAlert() {
+   alert("This Alert is Nothing LOL!");
+}
+
+function showConsole() {
+   console.log("Secret Message.");
+}
